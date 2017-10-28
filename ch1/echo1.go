a4ea57ce7094221e63c4c3825793fd987a6826dc
juniar_rakhman <juniar.rahman@gmail.com>
2017-10-29 00:58:20 +0700

    Initial commit

---
+++ ch1/echo1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s, sep := "",""
+	for i, arg := range os.Args[1:]{
+		s += sep + arg
+		sep = " "
+		fmt.Println("index: ", i)
+		fmt.Println("arg: " + arg)
+	}
+	fmt.Println(os.Args[0])
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
